6feee203588f821da053e0c866b816aa08e87a76
SS0117 <103245744+SS0117@users.noreply.github.com>
2023-03-07 08:12:58 +0000

    [Add]

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,9 +4,11 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :books, dependent: :destroy
-  
+  has_many :favorites, dependent: :destroy
+  has_many :book_comments, dependent: :destroy
+
   has_one_attached :profile_image
-  
+
   def get_profile_image(width, height)
     unless profile_image.attached?
       file_path = Rails.root.join('app/assets/images/sample-author1.jpg')
@@ -14,14 +16,14 @@ class User < ApplicationRecord
     end
     profile_image.variant(resize_to_limit: [width, height]).processed
   end
-  
+
   validates :name,
     presence: true,
     uniqueness: {case_sensitive: false},
     length: {minimum: 2, maximum: 20}
-    
+
   validates :introduction,
     length: {maximum: 50}
 
-  
+
 end
